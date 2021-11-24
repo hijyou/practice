47114a4c11e05f55094d4988e1a2d4261691faf6
hijyou <peppehitagi0607@docomo.ne.jp>
2021-11-25 01:10:18 +0900

    練習

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>templateでは、HTMLとVue構文を書く。</div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue"
+console.log("scriptでは、JSを書く")
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
